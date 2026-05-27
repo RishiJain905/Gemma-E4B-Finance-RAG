687dde315c7967bc1d26d427623f403b898245ed
LocalModelResearch Contributor <researcher@localmodel.dev>
2026-05-27 02:38:55 +0000

    1.2.3 - export ChromaStore from storage package (integration)

--- src/storage/__init__.py
+++ src/storage/__init__.py
@@ -2,5 +2,6 @@
 # Storage sub-package for Gemma-E4B-Finance-RAG
 
 from .sqlite_store import SQLiteStore
+from .chroma_store import ChromaStore
 
-__all__ = ["SQLiteStore"]
+__all__ = ["SQLiteStore", "ChromaStore"]
